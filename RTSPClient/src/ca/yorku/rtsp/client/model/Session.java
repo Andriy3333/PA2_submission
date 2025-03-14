0b4fe403542f0bd168157c6a75ff05e410293a8f
Andriy3333 <andriy.sapeha@gmail.com>
2025-03-14 17:16:11 -0400

    C

--- RTSPClient/src/ca/yorku/rtsp/client/model/Session.java
+++ RTSPClient/src/ca/yorku/rtsp/client/model/Session.java
@@ -10,8 +10,7 @@ package ca.yorku.rtsp.client.model;
 import ca.yorku.rtsp.client.exception.RTSPException;
 import ca.yorku.rtsp.client.net.RTSPConnection;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * This class manages an open session with an RTSP server. It provides the main interaction between the network
@@ -23,6 +22,16 @@ public class Session {
     private RTSPConnection rtspConnection;
     private String videoName = null;
 
+    // Buffering elements
+    private TreeSet<Frame> frameBuffer = new TreeSet<>();
+    private Timer playbackTimer = null;
+    private boolean isPlayingToUI = false;
+    private boolean endOfStream = false;
+    private Short lastPlayedSequence = null;
+
+    // Server state tracking
+    private boolean serverPlaying = false;
+
     /**
      * Creates a new RTSP session. This constructor will also create a new network connection with the server. No stream
      * setup is established at this point.
@@ -32,7 +41,6 @@ public class Session {
      * @throws RTSPException If it was not possible to establish a connection with the server.
      */
     public Session(String server, int port) throws RTSPException {
-
         rtspConnection = new RTSPConnection(this, server, port);
     }
 
@@ -63,10 +71,31 @@ public class Session {
      */
     public synchronized void open(String videoName) {
         try {
+            // Stop any existing playback
+            if (playbackTimer != null) {
+                playbackTimer.cancel();
+                playbackTimer = null;
+            }
+
+            // Reset all state
+            frameBuffer.clear();
+            isPlayingToUI = false;
+            endOfStream = false;
+            lastPlayedSequence = null;
+            serverPlaying = false;
+
+            // Setup the connection with the new video
             rtspConnection.setup(videoName);
             this.videoName = videoName;
-            for (SessionListener listener : sessionListeners)
+
+            // Start getting frames immediately (but don't display them)
+            rtspConnection.play();
+            serverPlaying = true;
+
+            // Notify listeners about the new video
+            for (SessionListener listener : sessionListeners) {
                 listener.videoNameChanged(this.videoName);
+            }
         } catch (RTSPException e) {
             listenerException(e);
         }
@@ -75,13 +104,29 @@ public class Session {
     /**
      * Starts to play the existing file. It should only be called once a file has been opened. This function will return
      * immediately after the request was responded. Frames will be received in the background and will be handled by
-     * the
-     * <code>processReceivedFrame</code> method. If the video has been paused previously, playback will resume where it
+     * the <code>processReceivedFrame</code> method. If the video has been paused previously, playback will resume where it
      * stopped.
      */
     public synchronized void play() {
+        if (videoName == null) return;
+
+        // Set flag to indicate we should be playing to UI
+        isPlayingToUI = true;
+
+        // Make sure server is sending frames if it's not already
         try {
-            rtspConnection.play();
+            if (!serverPlaying) {
+                rtspConnection.play();
+                serverPlaying = true;
+            }
+
+            // Start playback immediately if we have any frames available
+            if (!frameBuffer.isEmpty()) {
+                // Send first frame immediately
+                sendNextFrame();
+                // Then start the timer for subsequent frames
+                startPlayback();
+            }
         } catch (RTSPException e) {
             listenerException(e);
         }
@@ -93,8 +138,107 @@ public class Session {
      * the playback completely.
      */
     public synchronized void pause() {
+        // Just stop sending frames to UI - don't affect server
+        isPlayingToUI = false;
+
+        // Stop the playback timer if it's running
+        if (playbackTimer != null) {
+            playbackTimer.cancel();
+            playbackTimer = null;
+        }
+    }
+
+    /**
+     * Starts playback to UI at 25fps
+     */
+    private synchronized void startPlayback() {
+        // Cancel any existing timer
+        if (playbackTimer != null) {
+            playbackTimer.cancel();
+        }
+
+        // Create new timer to send frames to UI at 25fps (40ms intervals)
+        playbackTimer = new Timer();
+        playbackTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                sendNextFrame();
+            }
+        }, 40, 40);  // Start after 40ms, then every 40ms
+    }
+
+    /**
+     * Sends the next frame to the UI
+     */
+    private synchronized void sendNextFrame() {
+        // Check if we should stop
+        if (!isPlayingToUI || videoName == null) {
+            if (playbackTimer != null) {
+                playbackTimer.cancel();
+                playbackTimer = null;
+            }
+            return;
+        }
+
+        // Check for empty buffer
+        if (frameBuffer.isEmpty()) {
+            // Stop playback timer
+            if (playbackTimer != null) {
+                playbackTimer.cancel();
+                playbackTimer = null;
+            }
+
+            // If end of stream, notify listeners
+            if (endOfStream) {
+                for (SessionListener listener : sessionListeners) {
+                    listener.videoEnded();
+                }
+                return;
+            }
+
+            // Make sure server is sending frames
+            try {
+                if (!serverPlaying) {
+                    rtspConnection.play();
+                    serverPlaying = true;
+                }
+            } catch (RTSPException e) {
+                listenerException(e);
+            }
+            return;
+        }
+
+        // Get the next frame in sequence
+        Frame nextFrame = frameBuffer.pollFirst();
+        if (nextFrame != null) {
+            // Update last played sequence
+            lastPlayedSequence = nextFrame.getSequenceNumber();
+
+            // Send frame to UI
+            for (SessionListener listener : sessionListeners) {
+                listener.frameReceived(nextFrame);
+            }
+
+            // Check buffer levels
+            checkBufferLevels();
+        }
+    }
+
+    /**
+     * Check buffer levels and control server accordingly
+     */
+    private synchronized void checkBufferLevels() {
         try {
-            rtspConnection.pause();
+            // If buffer gets too full (100+ frames), pause server
+            if (frameBuffer.size() >= 100 && serverPlaying) {
+                rtspConnection.pause();
+                serverPlaying = false;
+            }
+            // If buffer gets too low (less than 80 frames), resume server
+            else if (frameBuffer.size() < 80 && !serverPlaying) {
+                rtspConnection.play();
+                serverPlaying = true;
+            }
         } catch (RTSPException e) {
             listenerException(e);
         }
@@ -105,53 +249,132 @@ public class Session {
      */
     public synchronized void close() {
         try {
+            // Stop playback timer
+            if (playbackTimer != null) {
+                playbackTimer.cancel();
+                playbackTimer = null;
+            }
+
+            // Reset state
+            isPlayingToUI = false;
+            frameBuffer.clear();
+            endOfStream = false;
+            lastPlayedSequence = null;
+            serverPlaying = false;
+
+            // Teardown connection
             rtspConnection.teardown();
-            processReceivedFrame(null);
             videoName = null;
-            for (SessionListener listener : sessionListeners)
-                listener.videoNameChanged(this.videoName);
+
+            // Notify listeners
+            for (SessionListener listener : sessionListeners) {
+                listener.videoNameChanged(null);
+            }
         } catch (RTSPException e) {
             listenerException(e);
         }
     }
 
-    private void listenerException(RTSPException e) {
-        for (SessionListener listener : sessionListeners)
+    /**
+     * Notifies listeners of an exception
+     */
+    private synchronized void listenerException(RTSPException e) {
+        for (SessionListener listener : sessionListeners) {
             listener.exceptionThrown(e);
+        }
     }
 
     /**
      * Closes the connection with the current server. This session element should not be used anymore after this point.
      */
     public synchronized void closeConnection() {
+        // Stop playback timer
+        if (playbackTimer != null) {
+            playbackTimer.cancel();
+            playbackTimer = null;
+        }
+
+        // Reset state
+        isPlayingToUI = false;
+
+        // Close connection
         rtspConnection.closeConnection();
     }
 
     /**
      * Processes a frame received from the RTSP server. This method
-     * will direct the frame to the user interface to be processed and
-     * presented to the user.
+     * will buffer the frame and manage playback according to buffer levels.
      *
      * @param frame The recently received frame.
      */
     public synchronized void processReceivedFrame(Frame frame) {
         if (videoName == null) return;
-        for (SessionListener listener : sessionListeners)
-            listener.frameReceived(frame);
+
+        if (frame == null) {
+            return;
+        }
+
+        // Check if empty frame (end of stream)
+        if (frame.getPayloadLength() == 0) {
+            endOfStream = true;
+
+            // If we're supposed to be playing and have no frames buffered,
+            // send the end notice to UI
+            if (isPlayingToUI && frameBuffer.isEmpty()) {
+                for (SessionListener listener : sessionListeners) {
+                    listener.videoEnded();
+                }
+            }
+            // If we're supposed to be playing but no timer running, start playback
+            else if (isPlayingToUI && playbackTimer == null && !frameBuffer.isEmpty()) {
+                sendNextFrame();  // Send first frame immediately
+                startPlayback();
+            }
+
+            return;
+        }
+
+        // Drop frames that come too late
+        if (lastPlayedSequence != null && frame.getSequenceNumber() <= lastPlayedSequence) {
+            return;
+        }
+
+        // Add frame to buffer
+        frameBuffer.add(frame);
+
+        // Check buffer levels
+        checkBufferLevels();
+
+        // If we're supposed to be playing and have enough frames but no timer running, start playback
+        if (isPlayingToUI && playbackTimer == null && frameBuffer.size() >= 50) {
+            sendNextFrame();  // Send first frame immediately
+            startPlayback();
+        }
     }
 
     /**
      * Processes a notification received from the RTSP server that the
-     * video ended. This method will direct the notification to the
-     * user interface to be handled as needed.
+     * video ended. This method will mark the stream as ended.
      *
      * @param sequenceNumber The sequence number for the end
      * notification. Corresponds to the last frame plus one. Can be
      * used to identify a missing frame at the end of the stream.
      */
     public synchronized void videoEnded(int sequenceNumber) {
-        for (SessionListener listener : sessionListeners)
-            listener.videoEnded();
+        endOfStream = true;
+
+        // If we're supposed to be playing and have no frames buffered,
+        // send the end notice to UI
+        if (isPlayingToUI && frameBuffer.isEmpty()) {
+            for (SessionListener listener : sessionListeners) {
+                listener.videoEnded();
+            }
+        }
+        // If we're supposed to be playing but no timer running, start playback
+        else if (isPlayingToUI && playbackTimer == null && !frameBuffer.isEmpty()) {
+            sendNextFrame();  // Send first frame immediately
+            startPlayback();
+        }
     }
 
     /**
--- RTSPClient/src/ca/yorku/rtsp/client/net/RTSPConnection.java
+++ RTSPClient/src/ca/yorku/rtsp/client/net/RTSPConnection.java
@@ -24,8 +24,18 @@ public class RTSPConnection {
     private static final int BUFFER_LENGTH = 0x10000;
     private final Session session;
 
+    // RTSP connection
+    private Socket rtspSocket;
+    private BufferedReader rtspReader;
+    private BufferedWriter rtspWriter;
 
-    // TODO Add additional fields, if necessary
+    // RTP connection
+    private DatagramSocket rtpSocket;
+    private String videoName;
+    private String sessionId;
+    private int sequenceNumber;
+    private RTPReceivingThread rtpThread;
+    private boolean isPlaying;
 
     /**
      * Establishes a new connection with an RTSP server. No message is
@@ -39,10 +49,18 @@ public class RTSPConnection {
      *                       are invalid or there is no connectivity.
      */
     public RTSPConnection(Session session, String server, int port) throws RTSPException {
-
         this.session = session;
+        this.sequenceNumber = 1;
+        this.isPlaying = false;
 
-        // TODO
+        try {
+            // Establish RTSP socket connection
+            rtspSocket = new Socket(server, port);
+            rtspReader = new BufferedReader(new InputStreamReader(rtspSocket.getInputStream()));
+            rtspWriter = new BufferedWriter(new OutputStreamWriter(rtspSocket.getOutputStream()));
+        } catch (IOException e) {
+            throw new RTSPException("Failed to establish RTSP connection: " + e.getMessage(), e);
+        }
     }
 
     /**
@@ -65,8 +83,45 @@ public class RTSPConnection {
      *                       response.
      */
     public synchronized void setup(String videoName) throws RTSPException {
+        try {
+            // Create RTP socket with random port
+            rtpSocket = new DatagramSocket();
+            rtpSocket.setSoTimeout(2000); // 2 second timeout
+            int rtpPort = rtpSocket.getLocalPort();
 
-        // TODO
+            // Save video name for later use
+            this.videoName = videoName;
+
+            // Build and send SETUP request
+            String setupRequest = "SETUP " + videoName + " RTSP/1.0\r\n" +
+                    "CSeq: " + sequenceNumber + "\r\n" +
+                    "Transport: RTP/UDP; client_port= " + rtpPort + "\r\n\r\n";
+
+            rtspWriter.write(setupRequest);
+            rtspWriter.flush();
+
+            // Read response
+            RTSPResponse response = readRTSPResponse();
+
+            // Check if response is successful (200 OK)
+            if (response.getResponseCode() != 200) {
+                rtpSocket.close();
+                throw new RTSPException("Setup failed: " + response.getResponseCode() + " " + response.getResponseMessage());
+            }
+
+            // Extract session ID from response
+            sessionId = response.getHeaderValue("Session");
+            if (sessionId == null) {
+                rtpSocket.close();
+                throw new RTSPException("No session ID provided in SETUP response");
+            }
+
+            // Increment sequence number for next request
+            sequenceNumber++;
+
+        } catch (IOException e) {
+            throw new RTSPException("Error during SETUP: " + e.getMessage(), e);
+        }
     }
 
     /**
@@ -82,11 +137,50 @@ public class RTSPConnection {
      *                       did not return a successful response.
      */
     public synchronized void play() throws RTSPException {
+        if (sessionId == null) {
+            throw new RTSPException("Cannot play: No session is set up");
+        }
 
-        // TODO
+        try {
+            // Build and send PLAY request
+            String playRequest = "PLAY " + videoName + " RTSP/1.0\r\n" +
+                    "CSeq: " + sequenceNumber + "\r\n" +
+                    "Session: " + sessionId + "\r\n\r\n";
+
+            rtspWriter.write(playRequest);
+            rtspWriter.flush();
+
+            // Read response
+            RTSPResponse response = readRTSPResponse();
+
+            // Check if response is successful (200 OK)
+            if (response.getResponseCode() != 200) {
+                throw new RTSPException("Play failed: " + response.getResponseCode() + " " + response.getResponseMessage());
+            }
+
+            // Increment sequence number for next request
+            sequenceNumber++;
+
+            // Start RTP receiving thread if not already playing
+            if (!isPlaying) {
+                isPlaying = true;
+                rtpThread = new RTPReceivingThread();
+                rtpThread.start();
+            }
+
+        } catch (IOException e) {
+            throw new RTSPException("Error during PLAY: " + e.getMessage(), e);
+        }
     }
 
     private class RTPReceivingThread extends Thread {
+        private boolean running = true;
+
+        public void stopRunning() {
+            running = false;
+            this.interrupt();
+        }
+
         /**
          * Continuously receives RTP packets until the thread is
          * cancelled or until an RTP packet is received with a
@@ -103,10 +197,42 @@ public class RTSPConnection {
          */
         @Override
         public void run() {
+            byte[] buffer = new byte[BUFFER_LENGTH];
 
-            // TODO
+            while (running) {
+                try {
+                    // Create datagram packet for receiving data
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+                    // Receive packet (will timeout after 2 seconds)
+                    rtpSocket.receive(packet);
+
+                    // Parse the RTP packet
+                    Frame frame = parseRTPPacket(packet);
+
+                    // Check if this is an end-of-stream packet (zero payload)
+                    if (frame.getPayload().length == 0) {
+                        session.videoEnded(sequenceNumber);
+                        running = false;
+                        break;
+                    }
+
+                    // Process the received frame
+                    session.processReceivedFrame(frame);
+
+                } catch (SocketTimeoutException e) {
+                    // Socket timeout - just continue trying
+                } catch (IOException e) {
+                    if (running) {
+                        System.err.println("Error receiving RTP packet: " + e.getMessage());
+                    }
+                    // If there's an error or if the socket is closed, exit the thread
+                    break;
+                }
+            }
+
+            isPlaying = false;
         }
-
     }
 
     /**
@@ -120,8 +246,40 @@ public class RTSPConnection {
      *                       did not return a successful response.
      */
     public synchronized void pause() throws RTSPException {
+        if (sessionId == null) {
+            throw new RTSPException("Cannot pause: No session is set up");
+        }
 
-        // TODO
+        try {
+            // Build and send PAUSE request
+            String pauseRequest = "PAUSE " + videoName + " RTSP/1.0\r\n" +
+                    "CSeq: " + sequenceNumber + "\r\n" +
+                    "Session: " + sessionId + "\r\n\r\n";
+
+            rtspWriter.write(pauseRequest);
+            rtspWriter.flush();
+
+            // Read response
+            RTSPResponse response = readRTSPResponse();
+
+            // Check if response is successful (200 OK)
+            if (response.getResponseCode() != 200) {
+                throw new RTSPException("Pause failed: " + response.getResponseCode() + " " + response.getResponseMessage());
+            }
+
+            // Increment sequence number for next request
+            sequenceNumber++;
+
+            // Stop RTP thread if it's running
+            if (isPlaying && rtpThread != null) {
+                rtpThread.stopRunning();
+                rtpThread = null;
+                isPlaying = false;
+            }
+
+        } catch (IOException e) {
+            throw new RTSPException("Error during PAUSE: " + e.getMessage(), e);
+        }
     }
 
     /**
@@ -139,8 +297,49 @@ public class RTSPConnection {
      *                       did not return a successful response.
      */
     public synchronized void teardown() throws RTSPException {
+        if (sessionId == null) {
+            throw new RTSPException("Cannot teardown: No session is set up");
+        }
 
-        // TODO
+        try {
+            // Build and send TEARDOWN request
+            String teardownRequest = "TEARDOWN " + videoName + " RTSP/1.0\r\n" +
+                    "CSeq: " + sequenceNumber + "\r\n" +
+                    "Session: " + sessionId + "\r\n\r\n";
+
+            rtspWriter.write(teardownRequest);
+            rtspWriter.flush();
+
+            // Read response
+            RTSPResponse response = readRTSPResponse();
+
+            // Check if response is successful (200 OK)
+            if (response.getResponseCode() != 200) {
+                throw new RTSPException("Teardown failed: " + response.getResponseCode() + " " + response.getResponseMessage());
+            }
+
+            // Increment sequence number for next request
+            sequenceNumber++;
+
+            // Stop RTP thread if it's running
+            if (isPlaying && rtpThread != null) {
+                rtpThread.stopRunning();
+                rtpThread = null;
+                isPlaying = false;
+            }
+
+            // Close RTP socket
+            if (rtpSocket != null && !rtpSocket.isClosed()) {
+                rtpSocket.close();
+                rtpSocket = null;
+            }
+
+            // Clear session information
+            sessionId = null;
+
+        } catch (IOException e) {
+            throw new RTSPException("Error during TEARDOWN: " + e.getMessage(), e);
+        }
     }
 
     /**
@@ -149,8 +348,35 @@ public class RTSPConnection {
      * such as the RTP connection and thread, if it is still open.
      */
     public synchronized void closeConnection() {
+        try {
+            // If a session is active, try to teardown first
+            if (sessionId != null) {
+                try {
+                    teardown();
+                } catch (RTSPException e) {
+                    System.err.println("Error during teardown in closeConnection: " + e.getMessage());
+                }
+            }
 
-        // TODO
+            // Close RTSP connection resources
+            if (rtspReader != null) {
+                rtspReader.close();
+                rtspReader = null;
+            }
+
+            if (rtspWriter != null) {
+                rtspWriter.close();
+                rtspWriter = null;
+            }
+
+            if (rtspSocket != null && !rtspSocket.isClosed()) {
+                rtspSocket.close();
+                rtspSocket = null;
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error closing RTSP connection: " + e.getMessage());
+        }
     }
 
     /**
@@ -163,11 +389,31 @@ public class RTSPConnection {
      * @return A Frame object.
      */
     public static Frame parseRTPPacket(DatagramPacket packet) {
+        byte[] data = packet.getData();
+        int offset = packet.getOffset();
+        int length = packet.getLength();
 
-        // TODO
-        return null;
+        if (length < 12) {
+            throw new IllegalArgumentException("RTP packet too short to be valid");
+        }
+
+        // RTP Header Parsing
+        int firstByte = data[offset] & 0xFF;
+        boolean marker = (data[offset + 1] & 0x80) != 0; // Marker bit
+        byte payloadType = (byte) (data[offset + 1] & 0x7F); // 7 bits for Payload Type
+
+        short sequenceNumber = (short) (((data[offset + 2] & 0xFF) << 8) | (data[offset + 3] & 0xFF));
+        int timestamp = ((data[offset + 4] & 0xFF) << 24) |
+                ((data[offset + 5] & 0xFF) << 16) |
+                ((data[offset + 6] & 0xFF) << 8) |
+                (data[offset + 7] & 0xFF);
+
+        // Payload extraction (excluding 12-byte RTP header)
+        int payloadOffset = offset + 12;
+        int payloadLength = length - 12;
+
+        return new Frame(payloadType, marker, sequenceNumber, timestamp, data, payloadOffset, payloadLength);
     }
-
 
     /**
      * Reads and parses an RTSP response from the socket's input. This
@@ -180,9 +426,42 @@ public class RTSPConnection {
      * @throws RTSPException If the response doesn't match the expected format.
      */
     public RTSPResponse readRTSPResponse() throws IOException, RTSPException {
+        // Read status line
+        String statusLine = rtspReader.readLine();
+        if (statusLine == null) {
+            return null; // End of stream
+        }
 
-        // TODO
-        return null; // Replace with a proper RTSPResponse
+        // Parse status line
+        String[] statusParts = statusLine.split(" ", 3);
+        if (statusParts.length < 3 || !statusParts[0].startsWith("RTSP/")) {
+            throw new RTSPException("Invalid RTSP response format: " + statusLine);
+        }
+
+        // Extract RTSP version, response code and message
+        String rtspVersion = statusParts[0];
+        int responseCode;
+        try {
+            responseCode = Integer.parseInt(statusParts[1]);
+        } catch (NumberFormatException e) {
+            throw new RTSPException("Invalid response code: " + statusParts[1]);
+        }
+        String responseMessage = statusParts[2];
+
+        // Create response object
+        RTSPResponse response = new RTSPResponse(rtspVersion, responseCode, responseMessage);
+
+        // Read headers
+        String line;
+        while ((line = rtspReader.readLine()) != null && !line.isEmpty()) {
+            int colonIndex = line.indexOf(':');
+            if (colonIndex > 0) {
+                String headerName = line.substring(0, colonIndex).trim();
+                String headerValue = line.substring(colonIndex + 1).trim();
+                response.addHeaderValue(headerName, headerValue);
+            }
+        }
+
+        return response;
     }
-
 }
